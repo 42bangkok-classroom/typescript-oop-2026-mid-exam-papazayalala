31c817b5043f21a119a2af98a3005b5d4003d5f6
papazayalala <68011086@kmitl.ac.th>
2026-01-21 15:37:07 +0700

    p03

--- src/practicals/p03/fizzbuzz.ts
+++ src/practicals/p03/fizzbuzz.ts
@@ -1,1 +1,21 @@
 // Write your code below
+const a : number =Number(process.argv[2]);
+if ( isNaN(a) || a <=0 ){
+}
+else{
+    for (let i =0 ; i<a;i++){
+        let b=i+1;
+        if(b%3==0 && b%5==0){
+            console.log("FizzBuzz");
+        }
+        else if(b%3==0){
+            console.log("Fizz");
+        }
+        else if(b%5==0){
+            console.log("Buzz");
+        }
+        else{
+            console.log(b);
+        }
+    }
+}
